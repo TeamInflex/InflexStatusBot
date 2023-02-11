cd72c24c984ceab21a5e3d3ed83a8f456b298da3
Team Inflex <TheTeamInflex@Gmail.com>
2023-02-11 18:23:43 +0530

    Add files via upload

---
+++ InflexStatus.py
@@ -0,0 +1,74 @@
+import asyncio
+import datetime
+
+import aioschedule
+import pytz
+from pyrogram import Client
+from pyrogram.errors import FloodWait
+
+from config import (API_HASH, API_ID, BOT_ADMIN_IDS, BOT_LIST,
+                    CHANNEL_OR_GROUP_ID, CHECK_DELAY, MESSAGE_ID,
+                    SESSION_STRING, TIME_ZONE)
+
+app = Client(
+    'BOT',
+    API_ID,
+    API_HASH,
+    session_string=SESSION_STRING,
+    in_memory=True
+)
+
+async def bot_check(bot_username):
+    """Checks if bot is online or not
+
+    Args:
+        bot_username (str): bot username to check
+
+    Returns:
+        str: bot status
+    """
+    try:
+        checker_status = await app.send_message(bot_username, "/start")
+        first_message_id = checker_status.id
+        await asyncio.sleep(5)
+        async for message in app.get_chat_history(bot_username, limit=1):
+            second_message_id = message.id
+        if first_message_id == second_message_id:
+            status = f"\n\n🤖 **Bot**: @{bot_username}\n🔴 Status : **Oꜰꜰʟɪɴᴇ** ❌"
+            for bot_admin_id in BOT_ADMIN_IDS:
+                if bot_admin_id.isnumeric():
+                    bot_admin_id = int(bot_admin_id)
+                try:
+                    await app.send_message(bot_admin_id, f"🚨 **Notification** 🚨\n\n» @{bot_username} is **Oꜰꜰʟɪɴᴇ** ❌")
+                except Exception as e:
+                    print(e)
+        else:
+            status = f"\n\n🤖 **Bot**: @{bot_username}\n🟢 Status : **Oꜰꜰʟɪɴᴇ** ✅"
+        await app.read_chat_history(bot_username)
+        return status
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+
+
+async def status_checker():
+    message = f"💡 **🔗 Wᴇʟᴄᴏᴍᴇ Tᴏ Iɴꜰʟᴇx Bᴏᴛ'ꜱ Sᴛᴀᴛᴜꜱ Cʜᴀɴɴᴇʟ .** 💡\n\n🔗 Tʜɪꜱ Iꜱ Lɪᴠᴇ Sᴛᴀᴛᴜꜱ Oꜰ Aʟʟ Iɴꜰʟᴇx Bᴏᴛꜱ. Tʜɪꜱ Mᴇꜱꜱᴀɢᴇ Kᴇᴇᴘꜱ Oɴ Uᴘᴅᴀᴛɪɴɢ Iɴ Eᴠᴇʀʏ 60 Mɪɴꜱ Wɪᴛʜ Lɪᴠᴇ Sᴛᴀᴛᴜꜱ Oꜰ Aʟʟ Iɴꜰʟᴇx Bᴏᴛꜱ Wʜᴇᴛʜᴇʀ Tʜᴇʏ Aʀᴇ Oɴʟɪɴᴇ / Oꜰꜰʟɪɴᴇ .\n\n"
+    for bot in BOT_LIST:
+        message += await bot_check(bot)
+    time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+    last_update = time.strftime("%d %b %Y at %I:%M %p")
+    message += f"\n\n🛂 Lᴀꜱᴛ Cʜᴇᴄᴋ : {last_update} ({TIME_ZONE})\n\n🟡 **Iᴛ Wɪʟʟ Bᴇ Uᴘᴅᴀᴛᴇᴅ Eᴠᴇʀʏ {CHECK_DELAY} Seconds ({int(CHECK_DELAY/60)} Minutes)**"
+    await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, message)
+    print(f"Last Check: {last_update}")
+                        
+async def main():
+    await app.start()
+    info = await app.get_me()
+    print(f'[INFO] @{info.username} STARTED!')
+    await status_checker()
+    aioschedule.every(CHECK_DELAY).seconds.do(status_checker)
+
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(10)
+        
+asyncio.get_event_loop().run_until_complete(main())
---
+++ README.md
@@ -0,0 +1,37 @@
+## Status Checker Userbot
+check your bot status automatically using userbot, simply and easy.
+
+## Mandatory Vars
+1. `API_ID` : Telegram API_ID, get it from my.telegram.org/apps
+2. `API_HASH` : Telegram API_ID, get it from my.telegram.org/apps
+3. `SESSION_STRING` : A pyrogram session string
+4. `BOT_LIST` : Your bot username list without "@" (Example: MakhusicBot OceanThiefBot)
+5. `CHANNEL_OR_GROUP_ID` : Your channel's or group's Telegram id (Example: -1001246808642)
+6. `MESSAGE_ID` : Telegram id of message from your channel or group (Example: 10)
+7. `BOT_ADMIN_IDS` : Bot admin id (Example: 123456 127890) ~ optional
+8. `TIME_ZONE`: Your time zone (Example: Asia/Kolkata)
+9. `CHECK_DELAY`: Delay between each check in seconds (Example: 3600)
+
+## Installation
+
+##### Use [This](https://github.com/M4hbod/Bot-Status-Checker/tree/pyrogram-version-1) branch if you are using pyrogram version 1
+
+### The Easy Way
+
+##### Tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/M4hbod/Bot-Status-Checker/tree/main)
+
+### The Hard Way
+
+```sh
+git clone --single-branch --branch main https://github.com/M4hbod/Bot-Status-Checker/
+cd Bot-Status-Checker
+pip install -r requirements.txt
+cp example.env .env
+--- EDIT .env values appropriately ---
+python main.py
+```
+
+### Credits
+- [levina](https://github.com/levina-lab) for his [status-checker](https://github.com/levina-lab/status-checker)
